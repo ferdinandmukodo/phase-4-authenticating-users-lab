9dd663cf01dc4f8df110b35973f07473c7a1d4ad
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-12-01 00:16:46 +0300

    Completed the lab

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: params[:username])
+    if user
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
+end
